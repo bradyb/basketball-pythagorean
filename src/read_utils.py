9220e63ef2b7fc5b99e3cbaddcf555b11aac98e7
bradyb <brady.ben12@email.com>
2022-01-14 00:03:17 -0800

    Adds reading utils for 528 elo files.

--- src/read_utils.py
+++ src/read_utils.py
@@ -1,31 +1,56 @@
 import csv
-import matplotlib.pylab as plt
 
+from copy import deepcopy
 from teams import Team
 
+
 def process_games(filename):
-    teams = get_teams_map('../data/teams.csv')
+    teams = get_teams_map("../data/teams.csv")
     with open(filename) as games_file:
         games = csv.DictReader(games_file)
         for game in games:
-            teams[game['HOME_TEAM_ID']].add_game(game)
-            teams[game['VISITOR_TEAM_ID']].add_game(game)
+            teams[game["HOME_TEAM_ID"]].add_game(game)
+            teams[game["VISITOR_TEAM_ID"]].add_game(game)
     return teams
 
+
 def get_teams_map(filename):
+    return get_teams_map_by_key(filename, "TEAM_ID")
+
+
+def get_teams_map_by_abbrev(filename):
+    return get_teams_map_by_key(filename, "ABBREVIATION")
+
+
+def get_teams_map_by_key(filename, key_str):
+    assert key_str == "TEAM_ID" or key_str == "ABBREVIATION"
     teams_map = {}
     with open(filename) as teams_file:
         teams = csv.DictReader(teams_file)
         for team in teams:
-            teams_map[team['TEAM_ID']] = Team(team['TEAM_ID'], team['ABBREVIATION'])
+            teams_map[team[key_str]] = Team(team["TEAM_ID"], team["ABBREVIATION"])
     return teams_map
 
-if __name__ == '__main__':
-    # Plot Chicago Bulls values.
-    teams_seasons = process_games('../data/games.csv')
-    chi_seasons = teams_seasons['1610612741']
-    order_seasons = chi_seasons.get_sorted_difference_list(13.91)
-    seasons, values = zip(*order_seasons)
-    plt.plot(seasons, values)
-    plt.show()
 
+def fix_538_names(teams):
+    teams["PHO"] = deepcopy(teams["PHX"])
+    del teams["PHX"]
+    teams["CHO"] = deepcopy(teams["CHA"])
+    del teams["CHA"]
+    teams["BRK"] = deepcopy(teams["BKN"])
+    del teams["BKN"]
+    return teams
+
+
+def process_538_csv(filename):
+    teams = fix_538_names(get_teams_map_by_abbrev("../data/teams.csv"))
+    with open(filename) as games_file:
+        games = csv.DictReader(games_file)
+        for game in games:
+            teams[game["team1"]].add_538_game(game)
+            teams[game["team2"]].add_538_game(game)
+    return teams
+
+
+if __name__ == "__main__":
+    process_538_csv("../data/nba_elo_latest.csv")
--- src/teams.py
+++ src/teams.py
@@ -11,13 +11,18 @@ class TeamSeason:
 
     def get_expected_percentage(self, exponent):
         points_for_exponent = self.points_for ** exponent
-        return points_for_exponent / (points_for_exponent + self.points_against ** exponent)
+        return points_for_exponent / (
+            points_for_exponent + self.points_against ** exponent
+        )
 
     def get_actual_expected_difference(self, exponent):
         return self.get_winning_percentage() - self.get_expected_percentage(exponent)
 
     def get_abs_difference(self, exponent):
-        return abs(self.get_winning_percentage() - self.get_expected_percentage(exponent))
+        return abs(
+            self.get_winning_percentage() - self.get_expected_percentage(exponent)
+        )
+
 
 class Team:
     def __init__(self, id, abbrev):
@@ -26,22 +31,25 @@ class Team:
         self.seasons = {}
 
     def get_sorted_difference_list(self, exponent):
-        diff_dict = {int(season): team_season.get_actual_expected_difference(exponent) for season, team_season in self.seasons.items()}
+        diff_dict = {
+            int(season): team_season.get_actual_expected_difference(exponent)
+            for season, team_season in self.seasons.items()
+        }
         return sorted(diff_dict.items())
 
     def add_game(self, game_dict):
-        season_str = game_dict['SEASON']
+        season_str = game_dict["SEASON"]
         if season_str not in self.seasons:
             self.seasons[season_str] = TeamSeason(season_str)
 
         team_season = self.seasons[season_str]
         team_season.total_games += 1
 
-        home_points = convert_points(game_dict['PTS_home'])
-        away_points = convert_points(game_dict['PTS_away'])
+        home_points = convert_points(game_dict["PTS_home"])
+        away_points = convert_points(game_dict["PTS_away"])
         home_win = home_points > away_points
 
-        if self.id == game_dict['HOME_TEAM_ID']:
+        if self.id == game_dict["HOME_TEAM_ID"]:
             team_season.points_for += home_points
             team_season.points_against += away_points
             team_season.wins += 1 if home_win else 0
@@ -49,9 +57,34 @@ class Team:
             team_season.points_for += away_points
             team_season.points_against += home_points
             team_season.wins += 1 if not home_win else 0
-    
+
+    def add_538_game(self, game_dict):
+        if game_dict["score1"] == "":
+            return
+
+        season_str = game_dict["season"]
+        if season_str not in self.seasons:
+            self.seasons[season_str] = TeamSeason(season_str)
+
+        team_season = self.seasons[season_str]
+        team_season.total_games += 1
+
+        team1points = convert_points(game_dict["score1"])
+        team2points = convert_points(game_dict["score2"])
+        team1win = team1points > team2points
+
+        if self.abbrev == game_dict["team1"]:
+            team_season.points_for += team1points
+            team_season.points_against += team2points
+            team_season.wins += 1 if team1win else 0
+        else:
+            team_season.points_for += team2points
+            team_season.points_against += team1points
+            team_season.wins += 1 if not team1win else 0
+
+
 def convert_points(points_str):
-    if points_str.endswith('.0'):
+    if points_str.endswith(".0"):
         return int(float(points_str))
     else:
         return int(points_str)
